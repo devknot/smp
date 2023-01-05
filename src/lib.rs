38caf4b4ad75b0d0bab943ed8f3ac9b75ccee2bc
Devknot <devknot.asm@gmail.com>
2023-01-05 14:15:14 -0400

    adicionado swap e o clear ao Shild

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,84 @@
+#![feature(allocator_api)]
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
+}
+/*
+pub struct Shield<Data, Heap = std::alloc::Global>
+where
+	Data: std::marker::Sized,
+	Heap: std::alloc::Allocator,
+{
+    ptr: *mut Data,
+    alloc: Heap,
+    addr: usize,
+}
+
+impl Shield {
+	fn new() {
+		std::mem::size_of::<Data>()
+	}
+}
+*/
+
+const BLOCK: usize = 256;
+
+pub struct Shield<Data>
+where
+	Data: std::marker::Sized + std::marker::Copy + Default,
+{
+	block: [Data; BLOCK],
+    addr: usize,
+}
+
+enum Error {
+	Address,
+}
+
+impl <Data> Shield<Data>
+where
+	Data: std::marker::Sized + std::marker::Copy + Default,
+{
+	fn new(addr: usize) -> Self {
+		Self{
+			block: [Data::default(); BLOCK],
+			addr,
+		}
+	}
+	
+	fn swap(&mut self, addr: usize) -> Result<usize, Error> {
+		
+		if addr >= BLOCK {
+			return Err(Error::Address);
+		}
+		
+		let dst: *mut Data = &mut self.block[addr] as *mut Data;
+		
+		unsafe {
+			std::ptr::write(dst, self.block[self.addr]);
+		}
+		
+		let tmp = self.addr;
+		
+		self.addr = addr;
+		
+		Ok(tmp)
+		
+	}
+	
+	fn clear(&mut self, addr: usize) -> Result<(), Error> {
+		if addr >= BLOCK {
+			return Err(Error::Address);
+		}
+		
+		let dst: *mut Data = &mut self.block[addr] as *mut Data;
+		
+		unsafe {
+			std::ptr::write(dst, Data::default());
+		}
+		
+		Ok(())
+	}
 }
 
 #[cfg(test)]
